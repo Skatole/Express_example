f3dbf3899e872cecd4c425e8ce59f5e63290f7da
Skatole <r.bencelaszlo9511@gmail.com>
2020-04-15 11:51:24 +0200

    04_15

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const morgan = require('morgan');
+
+// app.get('/', (req, res) => {
+//   res.sendStatus(200);
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(201).send('TADAAAM');
+// });
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+};
+
+const authetnticationMiddleware = (req, res, next) => {
+  console.log("Authentication");
+  res.locals.user = { username: "Woody" };
+  next();
+};
+
+const getRootHandlerer = (req, res) => {
+  console.log(req.path, req.method, req.query);
+  res.sendStatus(200);
+};
+
+const getUserHandler = (req, res) => {
+  console.log("params:", req.params);
+  // res.sendStatus(200);
+  res
+    .status(400)
+    .send(`Dear ${res.locals.user.username} ! It was a bad request`);
+};
+
+const redirectExampHandler = (req, res) => {
+  res.redirect("/;");
+};
+
+const createIssue = (req, res) => {
+  console.log(req.body);
+  res.sendStatus(201);
+}
+
+app.use(morgan('tiny'));
+app.use(express.json);
+app.use(loggerMiddleware);
+app.use(authetnticationMiddleware);
+app.get("/", getRootHandlerer);
+app.get("/user/:userId", getUserHandler);
+app.get("/redirect-example", redirectExampHandler);
+app.post('/issue', createIssue);
+app.listen(3000);
